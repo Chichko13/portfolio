f68b817fcabf72564033592d91781273c0ce4fdd
Chichko13 <alex.du13013@live.fr>
2025-04-09 02:41:06 +0200

    update

--- mon-portfolio/vite.config.js
+++ mon-portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/PORTFOLIO_P12/" : "./",
+  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "./",
   plugins: [react()],
 });
